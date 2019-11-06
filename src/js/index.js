fea9bfab6686ddb9a18928c1bc885861410fdbb1
romefar <vlad.romanovr@gmail.com>
2019-11-07 02:29:26 +0300

    structure change

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,28 @@
+import "../styles/styles.css";
+
 () => { 
     console.log("sadasdasd");
 };
+
+
+() => { 
+    console.log("99999999");
+};
+
+() => { 
+    console.log("22");
+};
+
 
 class Srd { 
     start = () => { 
         console.log("242424");
     };
 }
+
+
+class SARAWE { 
+  
+}
+
+
