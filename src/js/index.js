c3f4d8481774ea02c099c074d232cfcd3dc2ef27
romefar <vlad.romanovr@gmail.com>
2019-11-11 16:48:54 +0300

    enter point changed

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import "../styles/styles.css";
 import Draw from './Draw';
 
 
-const draw = new Draw(document.getElementById('canvas'), document.getElementById('canvas1'), document.querySelector('.outher_poly'));
+const draw = new Draw(document.getElementById('canvas'), document.getElementById('canvas1'), document.querySelector('.outher_poly'), document.getElementById('root'));
 
 document.querySelector(".controllers-container__inner_clip_button").addEventListener('click', () => draw.setInnerMode())
 document.querySelector(".controllers-container__outher_clip_button").addEventListener('click', () => draw.setOutherMode());
